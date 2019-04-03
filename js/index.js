2ebb1d385a0e6aca669c2d7ba945c2af5a4d200f
zjx <zoujxswg@163.com>
2019-04-03 19:23:52 +0800

    04-做完商品和nav，首页基本完成

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(function(){
     banner();
+    goodsList();
 })
 function banner() {
     $.ajax({
@@ -7,7 +8,7 @@ function banner() {
         dataType: 'json',
         url: 'http://157.122.54.189:9094/api/public/v1/home/swiperdata',
         success: function (result) {
-            console.log(result);
+            // console.log(result);
             var html = template('pyg_bannerTemp', result);
             $('.pyg_indexbanner').html(html);
             var listhtml=template('bannerListTemp',result);
@@ -16,6 +17,18 @@ function banner() {
             gallery.slider({
                 interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
             });
+        }
+    })
+};
+function goodsList() {
+    $.ajax({
+        type:'get',
+        dataType:'json',
+        url:'http://157.122.54.189:9094/api/public/v1/home/goodslist',
+        success:function(result) {
+            console.log(result);
+            var goodsHtml=template('goodslistTemp',result);
+            $('.pyg_goodsList').html(goodsHtml);
         }
     })
 }
